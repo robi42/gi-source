1ff87033f4006869a524bc08ed5b223c1d5550ab
Robert Thurnher <robert@thurnher.email>
2017-07-15 21:07:28 +0200

    Add some testing

--- src/test/kotlin/net/robi42/gisource/GiSourceApplicationTests.kt
+++ src/test/kotlin/net/robi42/gisource/GiSourceApplicationTests.kt
@@ -1,17 +1,28 @@
 package net.robi42.gisource
 
-import org.junit.Ignore
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.stream.annotation.Bindings
+import org.springframework.cloud.stream.messaging.Source
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
+import javax.inject.Inject
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner::class)
 @SpringBootTest class GiSourceApplicationTests {
 
-    @Ignore("Requires running Redis/Kafka")
+    @Inject
+    @Bindings(GreetingSource::class)
+    @Suppress("SpringKotlinAutowiring")
+    private lateinit var source: Source
+
     @Test fun `context loads`() {}
+
+    @Test fun `wires up source output channel`() {
+        assertThat(source.output()).isNotNull()
+    }
 
 }
