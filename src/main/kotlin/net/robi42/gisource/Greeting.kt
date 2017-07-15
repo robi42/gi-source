fbdb90b8ff663dd81d86efc4867dd83bd87fba1d
Robert Thurnher <robert@thurnher.email>
2017-07-15 21:50:58 +0200

    Cleanup

--- src/main/kotlin/net/robi42/gisource/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package net.robi42.gisource
-
-import java.time.Instant
-import java.time.Instant.now
-
-data class Greeting(
-        val text: String,
-        val timestamp: Instant = now()
-)
--- src/main/kotlin/net/robi42/gisource/GreetingSource.kt
+++ src/main/kotlin/net/robi42/gisource/GreetingSource.kt
@@ -5,6 +5,8 @@ import org.springframework.cloud.stream.messaging.Source
 import org.springframework.integration.annotation.InboundChannelAdapter
 import org.springframework.integration.annotation.Poller
 import java.lang.System.currentTimeMillis
+import java.time.Instant
+import java.time.Instant.now
 
 @EnableBinding(Source::class)
 class GreetingSource {
@@ -14,3 +16,8 @@ class GreetingSource {
     fun greet() = Greeting(text = "Hello world! The time is: ${currentTimeMillis()}")
 
 }
+
+data class Greeting(
+        val text: String,
+        val timestamp: Instant = now()
+)
